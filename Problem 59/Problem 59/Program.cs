1d2cb92a9849a157bda908cd007f1c0388391a99
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:46:04 +0200

    Add files via upload

---
+++ Problem 59/Problem 59/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch c = Stopwatch.StartNew();
+
+            //file
+            string pathFile = "p059_cipher.txt";
+
+            //read file
+            string lines = System.IO.File.ReadAllText(pathFile);
+
+            //
+            string[] keys = lines.Replace(" ", "").Split(',');
+            List<int> arrayKey = new List<int>();
+            List<int> array = new List<int>();
+            
+            //for (int i = 0; i < keys.Length; i++)
+            //{  
+            //    int key = Convert.ToInt32(keys[i]);                
+            //    arrayKey.Add(key);
+            //    if (arrayKey.Count == 3)
+            //    {
+            //        int n = 0;
+
+            //        for (int k = 0; k < arrayKey.Count; k++)
+            //        {
+            //            n ^= arrayKey[k];
+            //        }
+            //        array.Add(n);
+            //        arrayKey.RemoveRange(0, 3);
+            //    }
+               
+            //}
+
+            //char character = ' ';
+            //int sum = 0;
+            //foreach (int n in array)
+            //{
+            //    character = Convert.ToChar(n);                
+            //    Console.Write((char)character);
+            //    sum += n;
+            //}
+            //Console.WriteLine(sum);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Problem 60/Problem 60/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+            int limit = 100000;
+            List<int> prime = new List<int>();
+
+            try
+            {
+                while (number < limit)
+                {
+                    if(isPrime(number))
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Console.ReadLine();
+        }
+
+        static bool isPrime(long number)
+        {
+            for (long i = 2; i <= Math.Sqrt(number); i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+
+    }
+}
