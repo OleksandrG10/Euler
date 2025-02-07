20b4c98ed70cc03983f034a160e29dd880272280
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 12:46:50 +0200

    Add files via upload

---
+++ Problem3/Problem3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Numerics;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const long primeNumber = 600851475143;
+            var primeFactors = Enumerable.Range(2, Convert.ToInt32(Math.Sqrt(primeNumber)))
+                                          .Reverse()
+                                          .AsParallel()
+                                          .Where(g => primeNumber % g == 0 && isPrime(g))
+                                          .FirstOrDefault();
+            
+                        
+            
+            Console.WriteLine("Max = {0}", primeFactors);
+            Console.ReadLine();
+        }
+
+        //перевіряємо чи просте число 
+        static bool isPrime(long number)
+        {
+            for (long i = 2; i <= Math.Sqrt(number); i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+    }
+} 
