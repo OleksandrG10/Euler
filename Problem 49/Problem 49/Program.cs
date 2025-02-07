34ed90d5e73df0c0d021dce263d87fc855ca4dfe
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:40:33 +0200

    Add files via upload

---
+++ Problem 49/Problem 49/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> prime = new List<int>();
+            try
+            {
+                for (int i = 1000; i < 10000; i++)
+                {
+                    if (isPrime(i))
+                        prime.Add(i);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static bool isPrime(long number)
+        {
+            for (long i = 2; i <= Math.Sqrt(number); i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
