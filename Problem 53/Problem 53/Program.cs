ee99d291b50034fc8ba14ab4595be587e6caec32
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:41:46 +0200

    Add files via upload

---
+++ Problem 53/Problem 53/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem_53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BigInteger> myList = new List<BigInteger>();
+            try
+            {
+                for (int n = 2; n <= 100; n++)
+                {
+                    for (int k = 1; k < n; k++)
+                    {
+                        BigInteger c = Factorial(n) / (Factorial(k) * Factorial(n - k));
+                        if (c > 1000000)
+                        {
+                            myList.Add(c);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Count = {0}", myList.Count);
+            Console.ReadKey();
+        }
+        private static BigInteger Factorial(BigInteger n)
+        {
+            if (n > 1)
+                return n * Factorial(n - 1);
+            else
+                return 1;
+        }
+    }
+}
---
+++ Problem 56/Problem 56/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem_56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = string.Empty;
+            BigInteger sum = 0;
+            BigInteger maxSum = 0;
+
+            try
+            {
+                for (int a = 1; a < 100; a++)
+                {
+                    for (int b = 1; b < a; b++)
+                    {
+                        BigInteger power = BigInteger.Pow(a, b);
+                        result = Convert.ToString(power);
+
+                        foreach (char ch in result)
+                        {                            
+                            sum += BigInteger.Parse(ch.ToString());
+                        }
+
+                        if (sum > maxSum)
+                            maxSum = sum;
+
+                        sum = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Maximum sum = {0}", maxSum);
+            Console.ReadKey();
+        }
+    }
+}
