d0fc5e95d9a59afc44e3734858482b39f46332d1
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:26:08 +0200

    Add files via upload

---
+++ Problem 33/Problem 33/Problem 33/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int denproduct = 1;
+                int nomproduct = 1;
+                for (int i = 1; i < 10; i++)
+                {
+                    for (int den = 1; den < i; den++)
+                    {
+                        for (int nom = 1; nom < den; nom++)
+                        {
+                            if ((nom * 10 + i) * den == nom * (10 * i + den))
+                            {
+                                denproduct *= den;
+                                nomproduct *= nom;
+                                Console.WriteLine("({0}, {1})", nomproduct, denproduct);
+                            }
+                        }
+                    }
+                }
+                denproduct /= NSD(nomproduct, denproduct);
+                Console.WriteLine("The product of denomirators {0}", denproduct);
+                //for (int i = 99; i > 10; i--)
+                //{
+                //    for (int j = 99; j > 10; j--)
+                //    {
+                //        int f = j / i;
+                //        if (f < 1)
+                //        {
+                //            if (NSD(j,i) != 1)
+                //            {
+                //                int a = NSD(j, i);                                
+                //                Console.Write("{0}/{1}, NSD({2},{3}) = {4} ", j, i, j, i, a);
+                //                Console.WriteLine();
+                //                count++;
+                //                //nsd.Add(a);
+                //                //if (!fraction.ContainsKey(j))
+                //                //    fraction.Add(j, i);
+                //            }
+                //            //for (int k = 0; k < dividers.Length; k++)
+                //            //{
+                //            //    if ((i % dividers[k] == 0) && (j % dividers[k] == 0))
+                //            //    {
+                //            //        if(!fraction.ContainsKey(j))
+                //            //        fraction.Add(j, i);
+                //            //    }
+                //            //}
+                //        }
+                //    }
+                //}
+
+                
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException zero)
+            {
+                Console.WriteLine(zero.Message);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+        }
+
+        private static int NSD(int a, int b)
+        {
+            int x = 0;
+            int y = 0;
+
+            if (a > b)
+            {
+                x = a;
+                y = b;
+            }
+            else
+            {
+                x = b;
+                y = a;
+            }
+
+            while (x % y != 0)
+            {
+                int temp = x;
+                x = y;
+                y = temp % x;
+            }
+
+            return y;
+        }
+    }
+}
---
+++ Problem 34/Problem 34/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 1000000;
+            string str = "";
+            int sumFact;
+            int sumNumbers = 0;
+            List<int> list = new List<int>();
+            
+            for (int i = 3; i < n; i++)
+            {
+                sumFact = 0;
+                str = Convert.ToString(i);
+                for (int j = 0; j < str.Length; j++)
+                {
+                    sumFact += Factorial(int.Parse(str[j].ToString()));
+                }
+                if (sumFact == i)
+                {
+                    sumNumbers += i;
+                    list.Add(i);
+                }
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i].ToString() + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write("\nSum = {0}", sumNumbers);
+            Console.WriteLine();
+            Console.WriteLine("\nPress any key...");
+            Console.ReadKey();
+        }
+
+        private static int Factorial(int p)
+        {
+            if ((p == 0) || (p == 1))
+                return 1;
+            return p * Factorial(p - 1);
+        }
+    }
+}
