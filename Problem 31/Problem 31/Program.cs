064e356d61fb0d5ba260dd1f20b162f2792ddf83
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:24:58 +0200

    Add files via upload

---
+++ Problem 31/Problem 31/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = 200;
+            
+            //            
+            //The first version of the solution
+            //
+
+            //int ways = 0;
+            //for (int a = target; a >= 0; a -= 200)
+            //{
+            //    for (int b = a; b >= 0; b -= 100)
+            //    {
+            //        for (int c = b; c >= 0; c -= 50)
+            //        {
+            //            for (int d = c; d >= 0; d -= 20)
+            //            {
+            //                for (int e = d; e >= 0; e -= 10)
+            //                {
+            //                    for (int f = e; f >= 0; f -= 5)
+            //                    {
+            //                        for (int g = f; g >= 0; g -= 2)
+            //                        {
+            //                            ways++;
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            //
+            //The second version of the solution
+            //
+
+            int[] coinSizes = { 1, 2, 5, 10, 20, 50, 100, 200 };
+            int[] ways = new int[target + 1];
+            ways[0] = 1;            
+
+            for (int i = 0; i < coinSizes.Length; i++)
+            {
+                for (int j = coinSizes[i]; j <= target; j++)
+                {
+                    ways[j] += ways[j - coinSizes[i]];
+                }
+            }
+
+
+            string pound = "£";
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(pound + "2 can be generated in {0} number of ways", ways[200]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problem 32/Problem 32/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch c = Stopwatch.StartNew();
+            try
+            {
+                List<int> myList = new List<int>();
+                Regex rx = new Regex("(?!.*([1-9]).*\\1)^[1-9]{9}$");
+                Match match;
+                int multOne = 1;                
+                int product;
+                string strNumber = string.Empty;                
+                int sumPandigital = 0;
+
+                while (multOne < 2000)
+                {
+                    for(int multTwo = 1; multTwo < 2000; multTwo++)                  
+                    {
+                        product = multOne * multTwo;
+                        strNumber += Convert.ToString(multOne);
+                        strNumber += Convert.ToString(multTwo);
+                        strNumber += Convert.ToString(product);
+                        match = rx.Match(strNumber);
+                        if (match.Success)
+                        {
+                            if (!myList.Contains(product))
+                            {
+                                myList.Add(product);
+                                Console.WriteLine("{0} x {1} = {2}", multOne, multTwo, product);
+                            }                            
+                        }
+                        strNumber = "";                        
+                    }
+                    multOne++;
+                }
+
+                foreach(var n in myList)
+                {
+                    sumPandigital+= n;
+                }
+                Console.WriteLine("Sum = {0}", sumPandigital);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            c.Stop();
+            Console.WriteLine("\nTime is running program {0}", c.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
