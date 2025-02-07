852109d2c29f26a3a811acac6c68a41112c16392
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:10:47 +0200

    Add files via upload

---
+++ Problem 18/Problem 18/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace Problem_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch c = Stopwatch.StartNew();
+            string fileName = @"f:\Проекти\Project Euler\Problem 18\Triangle.txt";
+            int[,] inputTriangle = readInput(fileName);
+            int posSolutions = (int)Math.Pow(2, inputTriangle.GetLength(0) - 1);
+            int largestSum = 0;
+            int tempSum, index;
+
+            for (int i = 0; i <= posSolutions; i++)
+            {
+                tempSum = inputTriangle[0, 0];
+                index = 0;
+                for (int j = 0; j < inputTriangle.GetLength(0) - 1; j++)
+                {
+                    index = index + (i >> j & 1);
+                    tempSum += inputTriangle[j + 1, index];
+                }
+                if (tempSum > largestSum)
+                {
+                    largestSum = tempSum;
+                }
+            }
+
+            Console.Write("Large sum = {0}", largestSum);
+            Console.ReadKey();
+        }
+
+        private static int[,] readInput(string filename)
+        {
+            string line;
+            string[] linePieces;
+            int lines = 0;
+
+            StreamReader r = new StreamReader(filename);
+            while ((line = r.ReadLine()) != null)
+            {
+                lines++;
+            }
+
+            int[,] inputTriangle = new int[lines, lines];
+            r.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            int j = 0;
+            while ((line = r.ReadLine()) != null)
+            {
+                linePieces = line.Split(' ');
+                for (int i = 0; i < linePieces.Length; i++)
+                {
+                    inputTriangle[j, i] = int.Parse(linePieces[i]);
+                }
+                j++;
+            }
+            r.Close();
+            return inputTriangle;
+        }       
+    }
+}
---
+++ Problem 19/Problem 19/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //0 => 'Sunday',
+            //1 => 'Monday',
+            //2 => 'Tuesday',
+            //3 => 'Wednesday',
+            //4 => 'Thursday',
+            //5 => 'Friday',
+            //6 => 'Saturday'
+            
+            int y = 0;
+            int count = 0;
+            for (int years = 1901; years <= 2000; years++)
+            {
+                Console.Write("Years {0}:", years);
+                for (int month = 1; month <= 12; month++)
+                {
+                    y = GetWeekDay(years, month, 1);                    
+                    if (y == 0)
+                    {
+                        Console.Write(" month {0}", month);
+                        count++;
+                    }
+                }
+                Console.WriteLine();
+            }                
+            Console.Write("Count Sunday {0}", count);
+            Console.ReadLine();
+        }
+      
+        private static int GetWeekDay(int year, int month, int day)
+        {
+            //щомісяця компенсувати
+            int[] m = new int[] { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
+
+            //після лютого
+            int af = month > 2 ? 0 : 1;
+
+            //кожен четвертий рік є високосним, якщо він ділиться на 100, якщо він ділиться на 400
+            int w = 5 + (year - 1000) * 365 + (year - 1000 - af) / 4 - (year - 1000 - af) / 100 + (year - 1600 - af) / 400 + m[month - 1] + (day - 1);
+            
+            return w % 7;
+        }
+
+        private static void GetDay(int year, int month, int day)
+        {
+            string str = Convert.ToString(year);
+            string subYear = str.Substring(2, 2);
+
+            Console.WriteLine(subYear);
+        }
+    }
+}
