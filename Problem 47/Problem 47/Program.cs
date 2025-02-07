d7b4bcef4da3b5f09cd849bf7d24d6159df38376
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:36:21 +0200

    Add files via upload

---
+++ Problem 47/Problem 47/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> factors = new List<int>();
+            List<int> factor = new List<int>();
+            Dictionary<int, List<int>> myDict = new Dictionary<int, List<int>>();
+            Dictionary<int, int> countFactors = new Dictionary<int, int>();
+            int count = 0;
+
+            for (int i = 1000; i <= 9999; i++)
+            {
+                factors = FindFactor(i);                
+                foreach (var f in factors)
+                {
+                    for (int j = 0; j < factors.Count; j++)
+                    {
+                        if (f == factors[j])
+                        {
+                            if(!factor.Contains(factors[j]))
+                                factor.Add(f);
+                            factors.Remove(factors[j]);
+                            factors.Insert(j, 0);
+                            count++;
+                        }
+                    }
+                }
+                if (factors.Count == 4)
+                    myDict.Add(i, factors);
+            }
+
+            foreach (var pair in myDict)
+            {
+                Console.Write("{0} = ", pair.Key);
+                Console.Write("(");
+                
+                foreach(var p in pair.Value)
+                {
+                   
+                   
+                     Console.Write("{0}^{1}", p.ToString(), count);
+                   
+                    //Console.Write("{0}", p.ToString());
+                    if (pair.Value.Last() != p)
+                        Console.Write("x");
+                }
+                Console.Write(") ");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<int> FindFactor(int number)
+        {
+            List<int> result = new List<int>();
+            
+            while (number % 2 == 0)
+            {
+                result.Add(2);
+                number /= 2;            
+            }
+
+            int factor = 3;
+            while (factor * factor <= number)
+            {
+                if (number % factor == 0)
+                {
+                    result.Add(factor);
+                    number /= factor;
+                }
+                else
+                {
+                    factor += 2;
+                }
+            }
+
+            if (number > 1)
+                result.Add(number);
+
+            return result;
+
+        }
+    }
+}
---
+++ Problem 48/Problem 48/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace Problem_48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch time = Stopwatch.StartNew();
+
+            StringBuilder sb = new StringBuilder();
+            BigInteger number = 0;
+
+            for (BigInteger i = 1; i <= 1000; i++)
+            {
+                number += SelfPowers(i, i);
+            }
+            
+            sb.Append(number);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Self powers = ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(sb.ToString());                       
+            
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\nLast ten sequence numbers = " + sb.ToString(sb.Length - 10, 10));
+            Console.WriteLine();
+            time.Stop();            
+            Console.WriteLine("\nThe time run programm {0} ms", time.ElapsedMilliseconds);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nPress any keys...");
+            Console.ReadKey();
+        }
+
+        private static BigInteger SelfPowers(BigInteger n, BigInteger i)
+        {
+            BigInteger power = 1;
+            for (int j = 1; j <= i; j++)
+            {
+                power *= n;
+            }
+            return power;
+        }
+    }
+}
