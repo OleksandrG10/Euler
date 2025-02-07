984cd92dc4094a63703b59ad7aed81f50c5378b5
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:09:21 +0200

    Add files via upload

---
+++ Problem15/Problem15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = 20;
+            BigInteger s = 0;
+
+            s = Factorial(2 * n) / (Factorial(n) * Factorial(n));
+
+            Console.WriteLine("s = {0}", s);
+            Console.ReadLine();
+        }
+
+        static BigInteger Factorial(BigInteger n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
---
+++ Problem16/Problem16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 спосіб
+            int sum = 0;
+            BigInteger n = BigInteger.Pow(2, 1000);
+            while (n > 0)
+            {
+                sum += (int)(n % 10);
+                n /= 10;
+            }
+            
+            //2 спосіб
+            //BigInteger n = BigInteger.Pow(2, 1000).ToString().Aggregate(0, (total, next) => total + (int)Char.GetNumericValue(next));
+            
+            Console.Write("sum = {0}", sum);
+            Console.ReadLine();
+        }
+
+       
+    }
+}
