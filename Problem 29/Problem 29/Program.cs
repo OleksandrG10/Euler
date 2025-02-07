6ce42b9d41c841fa1049d91e6c6fab615ff30ec8
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:24:15 +0200

    Add files via upload

---
+++ Problem 29/Problem 29/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BigInteger> distinctTerms = new List<BigInteger>();
+
+            for (BigInteger a = 2; a <= 100; a++)
+            {
+                for (BigInteger b = 2; b <= 100; b++)
+                {
+                    BigInteger n = Powers(a, b);
+                    if (!distinctTerms.Contains(n))
+                        distinctTerms.Add(n);
+                }
+            }
+
+            for (int i = 0; i < distinctTerms.Count; i++)
+            {
+                Console.Write("{0} ", distinctTerms[i].ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("\nCount = {0}", distinctTerms.Count);
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static BigInteger Powers(BigInteger a, BigInteger b)
+        {
+            BigInteger power = 2;
+            for (int i = 2; i <= b; i++)
+            {
+                power *= a;
+            }
+            return power;
+        }
+    }
+}
---
+++ Problem 30/Problem 30/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            int number;
+            int sumPower = 0;
+            string str = "";
+
+            for (int i = 1; i < 1000000; i++)
+            {
+                str = Convert.ToString(i);
+                number = 0;
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    number += Power(int.Parse(str[j].ToString()), 5);
+                }
+
+                if ((i == number) && (i != 1))
+                {
+                    sumPower += number;
+                    myList.Add(number);
+                }
+            }
+            
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.Write(myList[i].ToString() + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("\nSum power = {0}", sumPower);
+            Console.WriteLine("\nPress any key...");
+            Console.ReadKey();
+        }
+
+        private static int Power(int j, int p)
+        {
+            int power = 1;
+            for (int i = 0; i < p; i++)
+            {
+                power *= j;
+            }
+
+            return power;
+        }
+    }
+}
