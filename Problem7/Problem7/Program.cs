b8d8402a4b8e7069c1453c5976c19517281929df
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 12:48:48 +0200

    Add files via upload

---
+++ Problem7/Problem7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 105000;
+            int count = 1;
+
+            for (long i = 2; i < n; i++)
+            { 
+                if(isPrime(i))
+                {
+                    //count++;
+                    Console.WriteLine("i = {0}  count = {1}", i, count++);
+                }
+            }
+            Console.ReadLine();                        
+        }
+
+        static bool isPrime(long number)
+        {
+            for (long i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
