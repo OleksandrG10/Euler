1c5b6210a85b295e6b86bd676f9ed90d640ff2ba
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:11:50 +0200

    Add files via upload

---
+++ Problem 20/Problem 20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = 100;
+            string fact;
+            int sum = 0;
+
+            fact = Convert.ToString(F(n));            
+            
+            for (int i = 0; i < fact.Length; i++)
+            {
+                sum += int.Parse(fact.Substring(i, 1));
+            }
+            Console.Write("Sum of the digits in the number 100! = {0}", sum);
+            Console.ReadLine();
+        }
+
+        static BigInteger F(BigInteger n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * F(n - 1);
+        }
+    }
+}
---
+++ Problem 21/Problem 21/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace Problem_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = 0;
+            int sum2 = 0;
+            int result = 0;
+            int x = 0;
+            
+            Stopwatch c = Stopwatch.StartNew();
+
+            for (int num = 1; num <= 10000; num++)
+            {
+                for (int factor = 1; factor < num; factor++)
+                {
+                    x = num % factor;
+                    if (x == 0)
+                        sum1 = sum1 + factor;
+                }
+                if (num != sum1)
+                {
+                    for (int factor = 1; factor < sum1; factor++)
+                    {
+                        x = sum1 % factor;
+                        if (x == 0)
+                            sum2 = sum2 + factor;
+                    }
+                }
+                if (num == sum2)
+                {
+                    result += num;
+                    Console.Write("{0}\t", num);
+                }
+                
+                sum1 = 0;
+                sum2 = 0;
+            }
+            c.Stop();
+            Console.Write("\nThe sum of all the amicable numbers under 10000. Sum = {0}", result);
+            Console.WriteLine("\nThe time run programm {0} ms", c.ElapsedMilliseconds);
+            Console.ReadLine();
+        }        
+    }
+}
