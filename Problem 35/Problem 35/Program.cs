531d79713a1a8ee854a4386387045e78e92c0fe3
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:26:49 +0200

    Add files via upload

---
+++ Problem 35/Problem 35/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+            int limit = 1000000;
+            List<int> circularPrime = new List<int>();
+            try
+            {
+                while (number < limit)
+                {
+                    if (isCircularPrime(number))
+                        circularPrime.Add(number);
+                    number++;
+                }
+                foreach (int n in circularPrime)
+                {
+                    Console.Write("{0} ", n);
+                }
+                Console.WriteLine("\nThe circular primes below {0}: {1}", limit, circularPrime.Count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+
+        }
+        private static bool isCircularPrime(int n)
+        {
+            int count = 0;
+            int t = n;
+            if (isPrime(n))
+            {
+                while (t > 0)
+                {
+                    count++;
+                    t /= 10;
+                }           
+                int number = n;
+
+                while (isPrime(number))
+                {
+                    int remainder = number % 10;
+                    int division = number / 10;
+                    number = (int)((Math.Pow(10, count - 1)) * remainder) + division;
+                    if (number == n)
+                        return true;
+                }
+            }
+            return false;
+        }
+        private static bool isPrime(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Problem 36/Problem 36/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem_36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int limit = 1000000;
+            BigInteger sumPol = 0;
+
+            for (int i = 1; i < limit; i++)
+            {
+                if (isPolindrom(i))
+                {
+                    string stemp = DecimalToBinary(i);
+                    if (isPolindrom(BigInteger.Parse(stemp)))
+                    {
+                        sumPol += i;
+                        Console.Write(i.ToString() + " ");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("\nSum = {0}", sumPol);
+            Console.WriteLine("\nPress any key...");
+            Console.ReadKey();
+        }
+
+        private static string DecimalToBinary(int number)
+        {
+            int remainder = 0;
+            string result = string.Empty;
+            while (number > 0)
+            {
+                remainder = number % 2;
+                number /= 2;
+                result = remainder.ToString() + result;
+            }
+
+            return result;
+        }
+
+        private static bool isPolindrom(BigInteger number)
+        {
+            BigInteger next = 0;
+            BigInteger pol = number;
+
+            while (pol != 0)
+            {
+                next = (next * 10) + (pol % 10);
+                pol /= 10;
+            }
+
+            if (number == next)
+                return true;
+            else
+                return false;
+        }
+    }
+}
