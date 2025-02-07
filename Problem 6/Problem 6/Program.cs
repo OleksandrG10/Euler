d83cde1b60d28b4dabfa258e8501227b75e6f7fc
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 12:53:23 +0200

    Add files via upload

---
+++ Problem 6/Problem 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sum of the numbers in power
+            double sumPowerTwo = 0;
+
+            //power of the sum
+            double powerTwoSum = 0;
+            double n = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumPowerTwo += Math.Pow(i, 2);
+                n += i;
+            }
+            powerTwoSum = Math.Pow(n, 2);
+
+            double diffBetweenSum = powerTwoSum - sumPowerTwo;
+            Console.WriteLine("Difference between the sum = {0}", diffBetweenSum);
+            Console.Write("\nPress any key...");
+            Console.ReadKey();
+        }
+    }
+}
