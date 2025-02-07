8e3a455f8eee0b6739f8996155e50027fdd22306
OleksandrG10 <44273725+OleksandrG10@users.noreply.github.com>
2025-02-07 13:08:36 +0200

    Add files via upload

---
+++ Problem 14/Problem 14/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Problem_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxValue = 1000000;
+            int position = 0;
+            long n;
+            
+            //лічильник
+            int count = 1;
+
+            //максимальний
+            int maxCount = 0;
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+
+            for (int i = 2; i <= maxValue; i++)
+            {
+                count = 0;                              
+                
+                for (n = i; n != 1;)
+                {
+                    if (n % 2 == 0)
+                    {
+                        n = n / 2;
+                        count++;
+                    }
+                    else
+                    {
+                        n = 3 * n + 1;
+                        count++;
+                    }                   
+                }         
+                
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    position = i;
+                }
+            }            
+            Console.WriteLine("\nMax count element {0}, position {1}", maxCount, position);
+
+            stopWatch.Stop();
+            Console.Write("\nThe programm run {0} ms", stopWatch.ElapsedMilliseconds);
+            Console.ReadLine();
+        }        
+    }
+}
---
+++ Problem 17/Problem 17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace Problem_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch c = Stopwatch.StartNew();
+            int count = 0;
+            //шях до файлу з символами
+            string filePath = @"e:\Проекти\Project Euler\Problem 17\Letters.txt";// Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\Letters.txt";           
+            
+            //читаємо з файла в рядок
+            string words = System.IO.File.ReadAllText(filePath);
+            
+            //читаємо з рядка в строковий масив, видаляючи при цьому пробіли і коми
+            string[] w = words.Replace(" ", "").Split(',');
+
+            foreach (string s in w)
+            {                
+                for (int i = 0; i < s.Length; i++)
+                {
+                    count++;
+                }
+            }
+            c.Stop();
+            Console.Write("Count letters = {0}", count);
+            Console.Write("\nRun time programming {0}", c.ElapsedMilliseconds);
+            Console.ReadLine();
+        }        
+    }
+}
